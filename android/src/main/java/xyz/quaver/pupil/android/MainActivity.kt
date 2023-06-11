eebbcb7814e10828b520ad7fe57ef64ac266dc8c
tom5079 <7948651+tom5079@users.noreply.github.com>
2023-06-10 22:51:35 -0700

    removed unnecessary theme

--- android/src/main/java/xyz/quaver/pupil/android/MainActivity.kt
+++ android/src/main/java/xyz/quaver/pupil/android/MainActivity.kt
@@ -3,16 +3,13 @@ package xyz.quaver.pupil.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
 import xyz.quaver.pupil.common.Pupil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Pupil()
-            }
+            Pupil()
         }
     }
 }
