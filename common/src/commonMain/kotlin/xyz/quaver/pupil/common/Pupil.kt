d8cadb4ed9af2e8126b432934deef9133b9a66ae
tom5079 <7948651+tom5079@users.noreply.github.com>
2023-06-11 11:13:30 -0700

    renamed theme package

--- common/src/commonMain/kotlin/xyz/quaver/pupil/common/Pupil.kt
+++ common/src/commonMain/kotlin/xyz/quaver/pupil/common/Pupil.kt
@@ -1,11 +1,11 @@
 package xyz.quaver.pupil.common
 
 import androidx.compose.runtime.Composable
-import xyz.quaver.pupil.common.ui.PupilTheme
+import xyz.quaver.pupil.common.theme.PupilTheme
 
 @Composable
 fun Pupil() {
     PupilTheme {
-        
+        SourceSelector()
     }
 }
